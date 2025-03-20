508bc5a1d0e946ddccbd7534d040b5ff6577698d
davidinovargas <davidinovargas@gmail.com>
2025-03-20 08:49:39 -0300

    Ajustado bugs e exibições de console

--- JogoDados/Program.cs
+++ JogoDados/Program.cs
@@ -1,31 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JogoDados
 {
     class JogoDeDados
     {
         static Random random = new Random();
-        static int posicaoJogador = 0, posicaoAdversario = 0;
+        static int posicaoJogador, posicaoAdversario;
         const int chegada = 30;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Inicio do Jogo");
-            Console.WriteLine("Enter para iniciar");
-            while (posicaoJogador < chegada && posicaoAdversario < chegada)
+            bool jogarNovamente;
+            do
             {
-                TurnoJogador();
-                if (posicaoJogador >= chegada) break;
-                TurnoComputador();
-            }
-            Console.WriteLine("________________________________");
-            Console.WriteLine(posicaoJogador >= chegada ? "         você venceu" : "         você perdeu");
-            Console.WriteLine("________________________________");
-            Console.ReadLine();
+                
+                ResetarJogo();
+                Console.Clear();
+                Console.WriteLine("________________________________");
+                Console.WriteLine("       Inicio do Jogo");
+                Console.WriteLine(" Pressione Enter para iniciar");
+                Console.WriteLine("________________________________");
+                Console.ReadLine();
+
+                while (posicaoJogador < chegada && posicaoAdversario < chegada)
+                {
+                    TurnoJogador();
+                    if (posicaoJogador >= chegada) break;
+                    TurnoComputador();
+                }
+
+                Console.WriteLine("________________________________");
+                Console.WriteLine(posicaoJogador >= chegada ? "         Você venceu!" : "         Você perdeu!");
+                Console.WriteLine("________________________________");
+
+                Console.WriteLine("Deseja jogar novamente? (S/N)");
+                jogarNovamente = Console.ReadLine().Trim().ToUpper() == "S";
+            } while (jogarNovamente);
+        }
+
+        static void ResetarJogo()
+        {
+            posicaoJogador = 0;
+            posicaoAdversario = 0;
         }
 
         static void TurnoJogador()
@@ -34,61 +50,89 @@ namespace JogoDados
             do
             {
                 turnoExtra = false;
+                Console.WriteLine("________________________________");
                 Console.WriteLine("\nSua vez");
                 Console.ReadLine();
                 int rolagem = RolarDado();
+                Console.WriteLine($"Valor no dado: {rolagem}");
                 NewPosicao(ref posicaoJogador, rolagem);
+
+                if (posicaoJogador >= chegada)
+                {
+                    posicaoJogador = chegada;
+                    return;
+                }
+
                 if (rolagem == 6)
                 {
-                    Console.WriteLine("você ganhou um turno extra");
+                    Console.WriteLine("Você ganhou um turno extra!");
                     turnoExtra = true;
                 }
-            }
-            while (turnoExtra);
-
-            }
-        
+            } while (turnoExtra);
+        }
 
         static void TurnoComputador()
         {
-            Console.WriteLine("\nVez do Computador");
-            int rolagem = RolarDado();
-            NewPosicao(ref posicaoAdversario, rolagem);
+            bool turnoExtra;
+            do
+            {
+                turnoExtra = false;
+                Console.WriteLine("\nVez do Computador");
+                int rolagem = RolarDado();
+                Console.WriteLine($"Valor no dado: {rolagem}");
+                NewPosicao(ref posicaoAdversario, rolagem);
+                Console.WriteLine("________________________________");
 
+                if (posicaoAdversario >= chegada)
+                {
+                    posicaoAdversario = chegada;
+                    return;
+                }
+
+                if (rolagem == 6)
+                {
+                    Console.WriteLine("O Computador ganhou um turno extra!");
+                    turnoExtra = true;
+                }
+            } while (turnoExtra);
         }
 
         static int RolarDado()
         {
             return random.Next(1, 7);
         }
+
         static void NewPosicao(ref int posicao, int rolagem)
         {
             posicao += rolagem;
-            Console.WriteLine($"valor no dado:{rolagem}\nposicão atual:{posicao}\n");
+
+            if (posicao >= chegada)
+            {
+                posicao = chegada;
+                return;
+            }
+
+            Console.WriteLine($"Posição atual: {posicao}\n");
 
             bool bonus = false, penalidade = false;
-            
-                if (posicao == 5 || posicao == 10 || posicao == 15)
-                {
-                    Console.WriteLine("Bonus +3");
-                    posicao += 3;
-                    bonus = true;
-                }
-                else if (posicao == 7 || posicao == 13 || posicao == 20)
-                {
-                    Console.WriteLine("penalidade -2");
-                    posicao -= 2;
-                    penalidade = true;
 
-                }
+            if (posicao == 5 || posicao == 10 || posicao == 15)
+            {
+                Console.WriteLine("Bônus +3");
+                posicao += 3;
+                bonus = true;
+            }
+            else if (posicao == 7 || posicao == 13 || posicao == 20)
+            {
+                Console.WriteLine("Penalidade -2");
+                posicao -= 2;
+                penalidade = true;
+            }
 
             if (bonus || penalidade)
             {
-                Console.WriteLine($"\nnova posição:{posicao}");
+                Console.WriteLine($"\nNova posição: {posicao}");
             }
         }
-
-
     }
-
 }
